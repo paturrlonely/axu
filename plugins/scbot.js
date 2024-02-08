081582da6cd61876197e2e2d9273bbe461124426
X M4ZE <159207241+XM4ZE@users.noreply.github.com>
2024-02-08 12:49:32 +0700

    main

---
+++ plugins/scbot.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, command }) => {
+	conn.reply(m.chat, `*I N F O   S C R I P T   Y U L A*
+
+*Multi Auth ( Support Pairing Code )*
+https://github.com/clicknetcafe/azamibot-md-multi
+
+*Recoded by Maximus*
+}
+
+handler.command = /^(sc|sourcecode|script)$/i
+
+export default handler

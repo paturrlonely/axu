9248b9cae2830aacdce183667bd57243d34f6c1e
X M4ZE <159207241+XM4ZE@users.noreply.github.com>
2024-02-09 10:13:41 +0700

    Update downloader-spotifysearch.js

--- plugins/downloader-spotifysearch.js
+++ plugins/downloader-spotifysearch.js
@@ -1,65 +1,41 @@
-/*let fetch = require('node-fetch')
+import fetch from 'node-fetch';
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-	if (!text) throw `Example: ${usedPrefix + command} Melukis Senja`
-    let res = await fetch(`https://api.lolhuman.xyz/api/spotifysearch?apikey=${global.lolkey}&query=${encodeURIComponent(text)}`)
-    conn.sendMessage(m.chat, {
-		react: {
-			text: '⏳',
-			key: m.key,
-		}
-	})
-    if (!res.ok) throw await `Invalid Spotify url / terjadi kesalahan.`
-    let json = await res.json()
-    if (json.status != '200') throw `Terjadi kesalahan, coba lagi nanti.`
-	let get_result = json.result
-	let ini_txt = `Found : *${text}*`
-	for (var x of get_result) {
-		ini_txt += `\n\n*Title : ${x.title}*\n`
-		ini_txt += `Artists : ${x.artists}\n`
-		ini_txt += `Duration : ${x.duration}\n`
-		ini_txt += `Link : ${x.link}\n\n`
-		ini_txt += `${x.preview_url ? `Preview : ${x.preview_url}\n` : ''}`
-		ini_txt += `───────────────────`
-	}
-	m.reply(ini_txt)
-}
+let handler = async (m, { text, conn, usedPrefix, command }) => {
+  if (!text) throw `*🚩 Contoh:* ${usedPrefix + command} Lathi`;  
+  let teks = '';
+  try {
+    const api = await fetch(`https://api.betabotz.eu.org/api/search/spotify?query=${text}&apikey=${lann}`);
+    let json = await api.json();
+    let res = json.result.data;    
+    for (let i in res) {
+      teks += `*${parseInt(i) + 1}.* *Title:* ${res[i].title}\n`;
+      teks += `*Duration:* ${res[i].duration}\n`;
+      teks += `*Popularity:* ${res[i].popularity}\n`;
+      teks += `*Link:* ${res[i].url}\n\n`;
+    }     
+    await conn.relayMessage(m.chat, {
+     extendedTextMessage:{
+                text: teks, 
+                contextInfo: {
+                     externalAdReply: {
+                        title: '',
+                        mediaType: 1,
+                        previewType: 0,
+                        renderLargerThumbnail: true,
+                        thumbnailUrl: 'https://www.scdn.co/i/_global/open-graph-default.png',
+                        sourceUrl: ''
+                    }
+                }, mentions: [m.sender]
+    }}, {})
+  } catch (e) {
+    throw `🚩 *Gagal Memuat Data!*`;
+  }
+};
 
-handler.help = ['spotsearch'].map(v => v + ' query')
-handler.tags = ['downloader', 'tools']
-handler.command = /^spot(ify)?search$/i
-handler.premium = false
-handler.limit = 5
-
-module.exports = handler*/
-
-import { spotify } from 'betabotz-tools';
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-	if (!text) throw `Example: ${usedPrefix + command} Melukis Senja`
-    let res = await spotify(text)
-    conn.sendMessage(m.chat, {
-		react: {
-			text: '⏳',
-			key: m.key,
-		}
-	})
-	let get_result = res.result.data
-	let ini_txt = `Found : *${text}*`
-	for (var x of get_result) {
-		ini_txt += `\n\n*Title : ${x.title}*\n`
-		ini_txt += `Artists : ${x.artist}\n`
-		ini_txt += `Duration : ${x.duration}\n`
-		ini_txt += `Link : ${x.url}\n\n`
-		ini_txt += `───────────────────`
-	}
-	m.reply(ini_txt)
-}
-
-handler.help = ['spotsearch'].map(v => v + ' <query>')
-handler.tags = ['downloader', 'tools']
-handler.command = /^spot(ify)?search$/i
-handler.premium = false
+handler.command = handler.help = ['spotifysearch'];
+handler.tags = ['downloader'];
+handler.premium = false;
+handler.group = false;
 handler.limit = true
 
-export default handler
+export default handler;

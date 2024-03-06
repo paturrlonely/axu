4e0a5d0901a5f89e71adcd6529a9f80fb0ffae36
X M4ZE <159207241+XM4ZE@users.noreply.github.com>
2024-03-06 09:35:11 +0700

    nomor owner mengikuti dari nomor yang tertera di global.owner ( config.js file )

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,12 +1,6 @@
 function handler(m) {
-  
-  const kontak = {
-	"displayName": 'Maximus Store',
-	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN: Maximus Store\nitem1.TEL;waid=6281283516246:6281283516246\nitem1.X-ABLabel:\nMy Owner\nURL;Email Owner: maximusstoreindonesia@gmail.com\nORG: JASA SEWABOT, PANEL & LAIN-LAIN\nEND:VCARD`
-}
-
-conn.sendMessage(m.chat, { contacts: { contacts: [kontak] }}, { quoted: m })
-  
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']

eeb17c70b1774bb42dea6feb66e8240cec88ab41
X M4ZE <159207241+XM4ZE@users.noreply.github.com>
2024-03-15 16:08:32 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,31 +1,34 @@
-import { tiktok } from 'betabotz-tools';
+import axios from 'axios';
 
-let handler = async (m, {
-	conn,
-	args,
-	usedPrefix,
-	command
-}) => {
-if (!args[0]) throw `Masukan URL!\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`;
-if (!args[0].match(/tiktok/gi)) throw `URL Yang Tuan Berikan *Salah!!!*`;
-try {
-let api = await tiktok(args[0]);
-let caption = `◥ *T I K T O K  D O W N L O A D* ◤
-
-• *Duration:* ${api.result.data.duration}
-• *Description:* ${api.result.data.title}
-
-${wm}`;
-await conn.sendMessage(m.chat, { video: { url: api.result.data.play }, caption: caption })
-await conn.sendFile(m.chat, api.result.data.music, 'kasar.mp3', null, m)
-      } catch (e) {
-		console.log(e)
-		m.reply(`Sistem Yula *Error*`)
-	}
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) throw `Masukan URL!\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`;    
+    try {
+        if (!text.match(/tiktok/gi)) throw `URL Tidak Ditemukan!`;        
+        m.reply(wait);      
+        const response = await axios.get(`https://api.botcahx.eu.org/api/dowloader/tiktok?url=${text}&apikey=${btc}`);        
+        const res = response.data.result;      
+        var { video, title, title_audio, audio } = res;
+        let capt = `乂 *T I K T O K*\n\n`;
+        capt += `◦ *Title* : ${title}\n`;
+        capt += `◦ *Audio* : ${title_audio}\n`;
+        capt += `\n`;        
+        await conn.sendFile(m.chat, video, null, capt, m);
+        conn.sendMessage(m.chat, { audio: { url: audio[0] }, mimetype: 'audio/mpeg' }, { quoted: m });         
+    } catch (e) {
+        console.log(e);
+        throw `🚩 ${eror}`;
+    }
 };
-handler.help = ['tiktok'].map(v => v + ' <url>');
+handler.help = ['tiktok'];
+handler.command = /^(tiktok|tt|tiktokdl|tiktoknowm)$/i
 handler.tags = ['downloader'];
-handler.command = /^(tiktok|tt|ttdl|tiktokdl)$/i;
 handler.limit = true;
-handler.register = false;
+handler.group = false;
+handler.premium = false;
+handler.owner = false;
+handler.admin = false;
+handler.botAdmin = false;
+handler.fail = null;
+handler.private = false;
+
 export default handler;

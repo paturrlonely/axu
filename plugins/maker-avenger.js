0372587362ceb4adc0fab35ed0f2ae9c30cca338
X M4ZE <159207241+XM4ZE@users.noreply.github.com>
2024-03-17 08:44:31 +0700

    Update maker-avenger.js

--- plugins/maker-avenger.js
+++ plugins/maker-avenger.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, args, usedPrefix, command  }) => {
   let teks = args.join(' ').split('|')
   if (!teks[0] || !teks[1]) throw `Masukan Text Nya!\n\nContoh\n${usedPrefix + command} The|Avengers`
-  let res = API('https://api.lolhuman.xyz', '/api/textprome2/avenger', { text1: teks[0], text2: teks[1] }, 'apikey')
+  let res = API('http://api.botcahx.eu.org', '/api/textpro/avengers-logo', { text: teks[0], text2: teks[1] }, 'apikey')
   conn.sendFile(m.chat, res, 'error.jpg', 'Ini Dia Kak', m, false)
 }
 handler.help = ['avengers']

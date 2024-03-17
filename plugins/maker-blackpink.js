7feec1bd9f96eecdd02f993bea0327316db66adb
X M4ZE <159207241+XM4ZE@users.noreply.github.com>
2024-03-17 08:46:44 +0700

    Update maker-blackpink.js

--- plugins/maker-blackpink.js
+++ plugins/maker-blackpink.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `Masukan Text Nya!\n\nContoh\n${usedPrefix + command} Blackpink`
-  let res = API('https://api.lolhuman.xyz', '/api/textprome/blackpink', { text: text }, 'apikey')
+  let res = API('http://api.botcahx.eu.org', '/api/textpro/black-pink', { text: text }, 'apikey')
   conn.sendFile(m.chat, res, 'error.jpg', 'Ini Dia Kak', m, false)
 }
 handler.help = ['blackpink']

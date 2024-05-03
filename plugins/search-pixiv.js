1962978d15b5baac8d9028b5387f24365038aa7c
X M4ZE <159207241+XM4ZE@users.noreply.github.com>
2024-05-03 14:48:55 +0700

    Update search-pixiv.js

--- plugins/search-pixiv.js
+++ plugins/search-pixiv.js
@@ -3,10 +3,18 @@ import axios from 'axios';
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   try {
 if (!text) throw `Contoh : \n.pixiv Eula Lawrence`
-const { data } = await axios.get(`https://api.itsrose.life/searching/pixiv/?mode=search&query=${text}&random=5&r18=9`, {
+
+const payloads = {
+  query: `${text}`,
+  enable_nsfw: true,
+  num_length: pickRandom(['1', '2', '3', '4', '5', '6', '7', '8']),
+  tag: ""
+};
+
+const { data } = await axios.post(`https://api.itsrose.life/pixiv/search/`, payloads, {
     headers: { Authorization: `${global.rose}` }
   }).catch((e) => e?.response);
-let img = data[Math.floor(Math.random() * data.length)].url;
+let img = data.result.results[Math.floor(Math.random() * data.result.results.length)].url;
         conn.sendFile(m.chat, img, 'img.png', `*Search results:* _${text}_`, m)
         	} catch (e) {
 		console.log(e)
@@ -20,3 +28,7 @@ handler.limit = true
 handler.nsfw = true
 
 export default handler
+
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}

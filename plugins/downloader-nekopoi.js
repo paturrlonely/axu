8977004adb25a0046b07d07624c68c44a4c991a2
X M4ZE <159207241+XM4ZE@users.noreply.github.com>
2024-05-09 17:00:59 +0700

    Update downloader-nekopoi.js

--- plugins/downloader-nekopoi.js
+++ plugins/downloader-nekopoi.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, command, text }) => {
   if (!text) throw `*Mana Code-nya?*\n\nSilahkan cari kode dengan menulis\n_${usedPrefix}nekopoi query_`
-  const { data } = await axios.get(`https://api.itsrose.life/dewasa/nekopoi/detail?id=${text}`, {
+  const { data } = await axios.get(`https://api.itsrose.rest/dewasa/nekopoi/detail?id=${text}`, {
     headers: { Authorization: `${global.rose}` }
   }).catch((e) => e?.response);
   if (data.status != true) throw `*Code Salah!!!*\n\nSilahkan cari kode dengan menulis\n_${usedPrefix}nekopoi query_`
@@ -26,7 +26,7 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 			caption: cap
 		}, { quoted: m })
 }
-handler.help = ['nekopoidl <code>']
+handler.help = ['nekopoidl *⧼code⧽*']
 handler.command = /^(nekopoidownload|nekopoidl|nekodl)$/i
 handler.tags = ['downloader','nsfw']
 

d846c54afb3fee0b0ca114205c8ca110f8d3b630
X M4ZE <159207241+XM4ZE@users.noreply.github.com>
2024-06-09 19:39:26 +0700

    Update internet-brainly.js

--- plugins/internet-brainly.js
+++ plugins/internet-brainly.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+/*import fetch from 'node-fetch'
 let handler = async (m, { text, command, usedPrefix }) => {
     if (!text) return m.reply(`Masukan Soal!\n\nContoh:\n${usedPrefix + command} Manusia Terbuat Dari Apa?`)
     let { result } = await (await fetch(API('lol', '/api/brainly', { query: text }, 'apikey'))).json()
@@ -15,4 +15,4 @@ Answer : ${v.answer.content.replace(/Jawaban(:)?/i, '')}
 handler.help = ['brainly <query>']
 handler.tags = ['internet']
 handler.command = /^brainly$/i
-export default handler
+export default handler*/

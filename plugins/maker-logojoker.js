fac1390d6b539a8a02e56cdb2ed9c3622736800a
X M4ZE <159207241+XM4ZE@users.noreply.github.com>
2024-06-09 19:44:09 +0700

    Update maker-logojoker.

--- plugins/maker-logojoker.js
+++ plugins/maker-logojoker.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+/*let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) return m.reply(`Masukan Format Dengan Benar!\n\nExample\n${usedPrefix + command} Joker`)
   let res = API('https://api.lolhuman.xyz', '/api/textprome/jokerlogo', { text: text }, 'apikey')
   conn.sendFile(m.chat, res, 'joker.jpg', 'Sudah Jadi', m, false)
@@ -9,4 +9,4 @@ handler.command = /^(logojoker)$/i
 
 handler.limit = true
 
-export default handler
+export default handler*/

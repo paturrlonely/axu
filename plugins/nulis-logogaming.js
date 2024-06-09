3dd34a0c6df48482fe8f8d9c8632a777f450e9c7
X M4ZE <159207241+XM4ZE@users.noreply.github.com>
2024-06-09 19:45:27 +0700

    Update nulis-logogaming.js

--- plugins/nulis-logogaming.js
+++ plugins/nulis-logogaming.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+/*let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) return m.reply(`Masukan Format Dengan Benar!\n\nExample\n${usedPrefix + command} Ruok`)
   let res = API('https://api.lolhuman.xyz', '/api/ephoto1/logogaming', { text: text }, 'apikey')
   conn.sendFile(m.chat, res, 'gaming.jpg', 'Ini Dia Kak', m)
@@ -7,4 +7,4 @@ handler.help = ['logogaming']
 handler.tags = ['nulis']
 handler.command = /^(logogaming)$/i
 handler.premium = true
-export default handler
+export default handler*/

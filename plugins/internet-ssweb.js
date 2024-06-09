65cb25eb8458bc513175e37990860d44037c8cf2
X M4ZE <159207241+XM4ZE@users.noreply.github.com>
2024-06-09 19:41:54 +0700

    Update internet-ssweb.js

--- plugins/internet-ssweb.js
+++ plugins/internet-ssweb.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args }) => {
    if (!args[0]) return conn.reply(m.chat, 'Linknya Mana Kak', m)
 m.reply(wait)
-let api = await fetch(`https://api.lolhuman.xyz/api/ssweb?apikey=${global.lolkey}&url=${args[0]}`)
+let api = await fetch(`https://api.betabotz.eu.org/api/tools/ssweb?url=${args[0]}&device=desktop&apikey=${global.lann}`)
 let data = await api.buffer()
 
    conn.sendMessage(m.chat, { image: data, caption: 'Here' }, { quoted: m })

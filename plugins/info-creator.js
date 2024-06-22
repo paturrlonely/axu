ffff27c3886ce3efdc0d019769738b9db2d3024b
X M4ZE <159207241+XM4ZE@users.noreply.github.com>
2024-06-22 12:22:41 +0700

    tinggal ganti di config

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,8 +1,8 @@
 function handler(m) {
   
   const kontak = {
-	"displayName": 'Maximus Store',
-	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN: Maximus Store\nitem1.TEL;waid=6281283516246:6281283516246\nitem1.X-ABLabel:\nMy Owner\nURL;Email Owner: maximusstoreindonesia@gmail.com\nORG: JASA SEWABOT, PANEL & LAIN-LAIN\nEND:VCARD`
+	"displayName": `${global.info.nameown}`,
+	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN: ${global.info.nameown}\nitem1.TEL;waid=${global.info.nomorown}:${global.info.nomorown}\nitem1.X-ABLabel:My Owner\n\nURL:maximusstoreindonesia@gmail.com\nORG: SEWABOT, PANEL\nEND:VCARD`
 }
 
 conn.sendMessage(m.chat, { contacts: { contacts: [kontak] }}, { quoted: m })

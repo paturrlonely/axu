7471273f7d958df91d242e9b2d904095f36e6b10
X <159207241+XM4ZE@users.noreply.github.com>
2024-07-01 20:12:35 +0700

    Update beton slide

--- plugins/search-pixiv.js
+++ plugins/search-pixiv.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-  try {
 if (!args[0]) throw `Contoh : \n.pixiv Eula Lawrence`
-
+try{
 const payloads = {
   query: `${args[0]}`,
   enable_nsfw: true,
@@ -14,17 +13,19 @@ const payloads = {
 const { data } = await axios.post(`https://api.itsrose.rest/pixiv/search/`, payloads, {
     headers: { Authorization: `${global.rose}` }
   }).catch((e) => e?.response);
-let img = data.result.results[Math.floor(Math.random() * data.result.results.length)].url;
-        conn.sendFile(m.chat, img, 'img.png', `*Search results:* _${text}_`, m)
+		let push = [];
+        for (let x of data.result.results.sort(() => Math.random() - 0.5))
+        push.push(['*Title:* ' + x.title + '\n*Author:* ' + x.author + '\n*ID:* ' + x.id || 'Untitled', global.wm, '', x.url, []])
+		await conn.sendButtonSlide(m.chat, `*Search of ${data.result.results.length} Result*`, '', push, m)
         	} catch (e) {
 		console.log(e)
 		throw `Pencarian Tidak Di Temukan!!!`
 	}
 }
 handler.help = ['pixivimg'].map(v => v + ' query')
-handler.tags = ['search', 'nsfw']
+handler.tags = ['search', 'nsfw', 'premium']
 handler.command = /^(pixiv|pixivimg)$/i
-handler.limit = true
+handler.premium = true
 handler.nsfw = true
 
 export default handler

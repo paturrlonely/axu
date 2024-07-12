2fe4cf23a3f319773a22a48798bce674fc02dcc1
X <159207241+XM4ZE@users.noreply.github.com>
2024-07-12 10:27:28 +0700

    Update youtube-ytv.js

--- plugins/youtube-ytv.js
+++ plugins/youtube-ytv.js
@@ -1,24 +1,23 @@
-import ytdl from 'ytdl-core'
-import { youtubedl } from '@bochilteam/scraper-sosmed'
+import ytdl from 'ytdl-core';
+import { youtubedl } from '@bochilteam/scraper-sosmed';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-	if (!(args[0] || '').match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))) return m.reply(`Invalid Youtube URL.`)
-	await m.reply(wait)
+	if (!(args[0] || '').match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))) return m.reply(`Example: *${usedPrefix}${command}* https://www.youtube.com/watch?v=K9_VFxzCuQ0`)
+	await m.reply(wait);
 	try {
-		let anu = await youtubedl(args[0])
+		let anu = await youtubedl(args[0]);
 		let list = Object.keys(anu.video).toString()
 		let data = anu.video[`${list.includes('36') ? '360p' : list.includes('24') ? '240p' : '144p'}`]
 		let url = await data.download()
-		if (data.fileSize > 400000) return m.reply(`Filesize: ${data.fileSizeH}\nTidak dapat mengirim, maksimal file 400 MB`)
+		if (data.fileSize > 50000) return m.reply(`Filesize: ${data.fileSizeH}\nTidak dapat mengirim, maksimal file 50 MB`)
 		let txt = `*${anu.title}*\n\n`
-		+ `⭔ Watch : ${args[0]}\n`
-		+ `⭔ Resolution : ${data.quality}\n`
-		+ `⭔ Size : ${data.fileSizeH}`
+		+ `⌲ Resolution : ${data.quality}\n`
+		+ `⌲ Size : ${data.fileSizeH}`
 		await conn.sendFile(m.chat, url, `${anu.title}.mp4`, txt, m)
 	} catch (e) {
 		console.log(e)
 		try {
-			let res = await ytdl.getURLVideoID(args[0])
+			let res = await ytdl.getURLVideoID(args[0]);
 			let anu = await ytdl.getInfo(res)
 			let data, det = anu.videoDetails
 			for (let x of ['360','480','240']) {
@@ -28,31 +27,17 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 			let size = parseInt(data.bitrate)
 			let buffer = Buffer.from(await (await fetch(data.url)).arrayBuffer())
 			let buffl = Buffer.byteLength(buffer)
-			if (size > 400000000) return m.reply(`Tidak dapat mengirim, maksimal file 400 MB`)
+			if (size > 50000000) return m.reply(`Tidak dapat mengirim, maksimal file 50 MB`)
 			let txt = `*${det.title}*\n\n`
-			+ `⭔ Watch : ${args[0]}\n`
-			+ `⭔ Resolution : ${data.width} x ${data.height}\n`
+			+ `⌲ Resolution : ${data.width} x ${data.height}\n`
+			+ `⌲ Watch : ${args[0]}\n`
 			await conn.sendFile(m.chat, buffer, `${det.title}.mp4`, txt, m)
 		} catch (e) {
 			console.log(e)
-			try {
-				let anu = await (await fetch(`https://rest-api.akuari.my.id/downloader/yt1?link=${args[0]}`)).json()
-				let size = anu.urldl_video.size
-				let vs = parseInt(size)
-				if (isNaN(vs)) vs = 1
-				if (vs > 400 && /mb|gb/i.test(size)) return m.reply(`Filesize: ${size}\nTidak dapat mengirim, maksimal file 400 MB`)
-				let txt = `*${anu.info.title}*\n\n`
-				+ `⭔ Watch : ${args[0]}\n`
-				+ `⭔ Resolution : ${anu.urldl_video.quality}\n`
-				+ `⭔ Size : ${size}`
-				await conn.sendFile(m.chat, anu.urldl_video.link, `${anu.info.title}.mp4`, txt, m)
-			} catch (e) {
-				console.log(e)
-				throw 'invalid url / internal server error.'
-			}
+			throw global.eror
 		}
 	}
-}
+};
 
 handler.menudownload = ['ytvideo <url>']
 handler.tagsdownload = ['downloader']

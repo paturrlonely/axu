6717a152862fda821117ab7cad7167bf570d4d96
X <159207241+XM4ZE@users.noreply.github.com>
2024-09-22 21:26:36 +0700

    Update internet-ssweb.js

--- plugins/internet-ssweb.js
+++ plugins/internet-ssweb.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args }) => {
    if (!args[0]) return conn.reply(m.chat, 'Linknya Mana Kak', m)
 m.reply(wait)
-let api = await fetch(`https://api.betabotz.eu.org/api/tools/ssweb?url=${args[0]}&device=desktop&apikey=${global.lann}`)
+let api = await fetch(`https://api.botcahx.eu.org/api/tools/ssweb?url=${args[0]}&device=desktop&apikey=${global.btc}`)
 let data = await api.buffer()
 
    conn.sendMessage(m.chat, { image: data, caption: 'Here' }, { quoted: m })
@@ -11,8 +11,7 @@ let data = await api.buffer()
 handler.help = ['ssweb <url>']
 handler.tags = ['internet']
 handler.command = /^(ssweb|ss)$/i
+handler.limit = true;
+handler.register = true;
 
-handler.limit = true
-handler.fail = null
-
-export default handler
+export default handler;

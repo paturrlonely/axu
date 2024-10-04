38ab4e22da80beb486b4bfe2862e1b79f97de96e
X <159207241+XM4ZE@users.noreply.github.com>
2024-10-04 20:18:35 +0700

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,10 +1,13 @@
-import { spawn } from 'child_process'
+import { exec } from 'child_process'
+
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (global.conn.user.jid == conn.user.jid) {
     await m.reply('```R E S T A R T . . .```')
-    process.send('reset')
-  } else throw '_eeeeeiiittsssss..._'
+restartIndexJs();
+
+process.exit(0);
+} else throw 'wew'
 }
 
 handler.help = ['restart']
@@ -14,3 +17,13 @@ handler.command = /^(res(tart)?)$/i
 handler.rowner = true
 
 export default handler
+
+function restartIndexJs() {
+
+  exec('node index.js', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error restarting index.js: ${error}`);
+      return;
+    }
+  });
+}

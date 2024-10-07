f783a498934a36f8f75fe5304175e720e9e5bcc9
X <159207241+XM4ZE@users.noreply.github.com>
2024-10-07 08:32:31 +0700

    Update _autodownload.js

--- plugins/_autodownload.js
+++ plugins/_autodownload.js
@@ -4,7 +4,7 @@ async function downloadTikTok(link, m) {
   try {
     const response = await fetch(`https://api.botcahx.eu.org/api/dowloader/tiktok?url=${link}&apikey=${btc}`);
     const data = await response.json();
-    await conn.sendFile(m.chat, data.result.video, null, 'Tiktok Download', m);
+    await conn.sendFile(m.chat, data.result.video, null, `*Tiktok Download*`, m);
   } catch (error) {
     console.error(error);
   }
@@ -16,6 +16,7 @@ async function downloadInstagram(link, m) {
     const data = await response.json();
 
     const limit = 3;
+    
     const promises = data.result.slice(0, limit).map(image => {
       return conn.sendFile(m.chat, image.url, null, `*Instagram Downloader*`, m);
     });
@@ -32,6 +33,7 @@ async function downloadFacebook(link, m) {
     const response = await fetch(`https://api.botcahx.eu.org/api/dowloader/fbdown3?url=${link}&apikey=${btc}`);
     const data = await response.json();
     const urls = data.result.url.urls;
+    const liimit = global.db.data.users[m.sender].limit
 
     if (Array.isArray(urls)) {
       for (let url of urls) {
@@ -47,10 +49,15 @@ async function downloadFacebook(link, m) {
   }
 }
 
-export async function before(m, { isPrems }) {
+export async function before(m, { conn, isPrems }) {
   let chat = global.db.data.chats[m.chat];
-
+  let user = global.db.data.users[m.sender];
+  
   if (!m.text && m.fromMe) {
+    return;
+  }
+  
+  if (!isPrems) {
     return;
   }
 
@@ -61,6 +68,10 @@ export async function before(m, { isPrems }) {
   if (chat.isBanned) {
     return;
   }
+  
+  if (!m.text.includes('http')) {
+    return;
+  }
 
   let text = m.text.replace(/\n+/g, ' ');
 
@@ -69,12 +80,30 @@ export async function before(m, { isPrems }) {
   const facebookRegex = /^(?:https?:\/\/(web\.|www\.|m\.)?(facebook|fb)\.(com|watch)\S+)?$/i;
 
   if (text.match(tiktokRegex)) {
+    conn.sendMessage(m.chat, {
+		react: {
+			text: '🌐',
+			key: m.key,
+		}
+	})
     await downloadTikTok(text.match(tiktokRegex)[0], m);
   } else if (text.match(instagramRegex)) {
+    conn.sendMessage(m.chat, {
+		react: {
+			text: '🌐',
+			key: m.key,
+		}
+	})
     await downloadInstagram(text.match(instagramRegex)[0], m);
   } else if (text.match(facebookRegex)) {
+    conn.sendMessage(m.chat, {
+		react: {
+			text: '🌐',
+			key: m.key,
+		}
+	})
     await downloadFacebook(text.match(facebookRegex)[0], m);
   }
-
+  
   return !0;
 }

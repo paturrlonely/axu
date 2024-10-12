de35de0860838acd7c0d1a24812ea65fcf883cab
X <159207241+XM4ZE@users.noreply.github.com>
2024-10-12 07:39:27 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1511,7 +1511,7 @@ global.dfail = (type, m, conn) => {
 		game: 'This Command Has Not Been Activated In This Group.\n\n> Enable This Feature By Writing *.enable game*',
 		xmaze: 'This group does not allow this feature to be used. please join this group:\nhttps://chat.whatsapp.com/FJRtTzRKxP8A2wT6fcCW3s\n\n disable this feature by writing .enable allfitur',
 		restrict: '*FEATURES TURNED OFF BY OWNERS*',
-		unreg: '*Indonesian*\nSilahkan daftar untuk menggunakan fitur ini dengan cara mengetik:\n\n*#daftar nama.umur*\n\nContoh: *#daftar Maximus.19*\n══════════════════════\n*English*\nPlease register to use this feature by typing:\n\n*#register name.age*\n\nExample: *#register Maximus.19*'
+		unreg: '*Please register first by writing \`#register name.age\`\n* Once you are registered, then you can use this command.\n\n*Registration example:*\n\n\`#register Mulyono.45\`\n\n*Notes:*\n* Make sure you write your name and age correctly.\n* You only need to register once.'
 	} [type]
 
 	if (msg) return conn.sendMessage(m.chat, {

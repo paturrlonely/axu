5f98e097a1a9d506129fc5b87e8088a5c172d055
X <159207241+XM4ZE@users.noreply.github.com>
2024-11-14 15:54:36 +0700

    Update info-sewa.js

--- plugins/info-sewa.js
+++ plugins/info-sewa.js
@@ -1,26 +1,5 @@
-import { getDevice } from '@adiwajshing/baileys'
-
 let handler = async(m, { conn, text, usedPrefix, command}) => {
-    let xm4ze = await( await fetch(xmenus)).json()
-    let thum = xm4ze[Math.floor(Math.random() * xm4ze.length)]
-    let pesan = `\n> *1.* 30 Day join the group\n> *2.* 3 Day Premium\n> *3.* 30 Day Premium\n\nSilahkan pilih produk kami dengan mengeklik tombol dibawah ini`
-    const sections = [
-				[
-					'— YULA JOIN GROUP', [
-						['Harga: Rp. 7.000 IDR', '.sewa G7', '7 Day join the group'],
-						['Harga: Rp. 15.000 IDR', '.sewa G30', '30 Day join the group'],
-						['Harga: Rp. 30.000 IDR', '.sewa G60', '60 Day join the group']
-				]], [
-					'— PREMIUM USER', [
-						['Harga: Rp. 5.000 IDR', '.sewa 3day', '3 Day Premium'],
-						['Harga: Rp. 7.000 IDR', '.sewa 7day', '7 Day Premium'],
-						['Harga: Rp. 30.000 IDR', '.sewa 30day', '30 Day Premium'],
-						['Harga: Rp. 60.000 IDR', '.sewa 60day', '60 Day Premium'],
-				]],
-			]
-    if (!/all/.test(command) && await getDevice(m.key.id) == 'android') {
-    if (!text) return conn.sendButtonList(m.chat, `— TOP 3 SELLING PRODUCTS`, pesan, global.wm, 'PRICE LIST', thum, sections, m)
-    }
+    if (!text) return conn.reply(m.chat, `*— YULA JOIN GROUP*\n\n*7 Day join the group*\n- Harga: Rp. 7.000 IDR\n- ID Pesanan: G7\n\n*30 Day join the group*\n- Harga: Rp. 15.000 IDR\n- ID Pesanan: G30\n\n*60 Day join the group*\n- Harga: Rp. 30.000 IDR\n- ID Pesanan: G60\n\n\n*— PREMIUM USER*\n\n*3 Day premium*\n- Harga: Rp. 5.000 IDR\n- ID Pesanan: 3day\n\n*7 Day premium*\n- Harga: Rp. 10.000 IDR\n- ID Pesanan: 7day\n\n*30 Day premium*\n- Harga: Rp. 15.000 IDR\n- ID Pesanan: 30day\n\n*60 Day premium*\n- Harga: Rp. 30.000 IDR\n- ID Pesanan: 60day\n\n\n*Example:* ${usedPrefix + command} <ID Pesanan>\n*Example:* ${usedPrefix + command} 30day`, m)
     let orderID;
 
   switch(text) {
@@ -80,20 +59,16 @@ let handler = async(m, { conn, text, usedPrefix, command}) => {
 *60 Day premium*
 - Harga: Rp. 30.000 IDR
 - ID Pesanan: 60day
-
-
-*Example:* ${usedPrefix + command} <ID Pesanan>
-*Example:* ${usedPrefix + command} 30day
 `;
   };
-    let maximus = `*Ｋ Λ Ｒ Λ  O R D E R*\n\n*• Status:* _🟡 Pending_\n*• ID Pembelian:* ${text}\n*• Nominal:* ${orderID}\n*• Payment:* QRIS\n\n\n*TAHAP-TAHAP Pembayaran*\n*1.* Silahkan SCAN QRIS ini dengan M-Banking/E-Wallet kamu\n*2.* Masukan Nominal: ${orderID}\n*3.* Silahkan Kirim bukti pembayaran ke nomor ini wa.me/${global.info.nomorown}`
+    let maximus = `*Y U L A  O R D E R*\n\n*• Status:* _🟡 Pending_\n*• ID Pembelian:* ${text}\n*• Nominal:* ${orderID}\n*• Payment:* QRIS\n\n\n*TAHAP-TAHAP Pembayaran*\n*1.* Silahkan SCAN QRIS ini dengan M-Banking/E-Wallet kamu\n*2.* Masukan Nominal: ${orderID}\n*3.* Silahkan Kirim bukti pembayaran ke nomor ini wa.me/${global.info.nomorown}`
     conn.sendFile(m.sender, global.qris, 'maxim.jpg', maximus, m)
     conn.reply(m.chat, '✔️ *PESANANMU TELAH DI BUAT*\n\nSaya telah mengirim pembayarannya melalu private chat silahkan di baca dan ikuti tahap-tahap pembayaran. Terimakasih', m)
     conn.reply(global.info.nomorown + '@s.whatsapp.net', `@${m.sender.split('@')[0]} Sedang dalam pembayaran nominal ${orderID}`, m, { contextInfo: { mentionedJid: [m.sender] }})
 }
 handler.help = ['sewa', 'order']
 handler.tags = ['info']
-handler.command =  /(order|sewa)/i
+handler.command =  /(order|sewa|premium)/i
 handler.register = false;
 handler.premium = false;
 

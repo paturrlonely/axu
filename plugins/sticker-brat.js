a15570c9d47b0e89b6196ca63d51d50127ec8b59
X <159207241+XM4ZE@users.noreply.github.com>
2024-11-17 17:47:06 +0700

    Update plugins sticker-brat.js

---
+++ plugins/sticker-brat.js
@@ -0,0 +1,39 @@
+import {
+	Sticker
+} from 'wa-sticker-formatter'
+var handler = async (m, {
+	conn,
+	text,
+	command
+}) => {
+    if (!text) m.reply('Masukan text')
+	try {
+        const emojis = text.match(/[\p{Emoji}\uFE0F-\uFFFF]/gu);
+        let emojiText = emojis ? emojis.join('') : '';
+        const maxTextLength = 151 - emojiText.length;  // Mengurangi panjang emoji dari batas teks
+        let clippedText = text.substring(0, maxTextLength);
+
+        let req = `https://api.botcahx.eu.org/api/maker/brat?text=${encodeURIComponent(clippedText)}&apikey=${lann}`
+        let stiker = await createSticker(req, false, '', '')
+    	await conn.sendFile(m.chat, stiker, '', '', m, '')
+	} catch (e) {
+		console.log(e)
+	}
+}
+
+handler.command = handler.help = ['mmk']
+handler.tags = ['sticker']
+handler.limit = true;
+handler.register = true;
+
+export default handler;
+
+async function createSticker(img, url, packName, authorName, quality) {
+	let stickerMetadata = {
+		type: 'full',
+		pack: stickpack,
+		author: stickauth,
+		quality
+	}
+	return (new Sticker(img ? img : url, stickerMetadata)).toBuffer()
+}
